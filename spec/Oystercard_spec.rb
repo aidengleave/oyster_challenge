0fe74f3c7c85e2ca866f98fdeedc0c776226b0d3
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-14 18:16:29 +0000

    Test: card can be topped up with a value

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -2,6 +2,7 @@ require 'Oystercard'
 
 
 RSpec.describe Oystercard do
+
   it 'Card has a default balance of 0' do
     card = Oystercard.new
     expect(card.balance).to eq(0)
@@ -10,17 +11,15 @@ RSpec.describe Oystercard do
 context '#top_up' do
   it 'checks that if a top_up method is invoked with the top up value as the argument' do
     card = Oystercard.new
-    amount = double('some value')
+    amount = 1
     expect(card.top_up(amount))
   end
-  
+
   it 'can be topped up' do
     card = Oystercard.new
     amount = 1
-    card.balance
-    expect(card.top_up(amount)).to eq((card.balance) + amount)
+    expect(card.top_up(amount)).to eq(amount)
   end
 end
-
 
 end
