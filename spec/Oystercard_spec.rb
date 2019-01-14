ecdde1b1b752a3140f274e1577581a151b17d457
KrzysztofBalejko <chrisbalejko86@gmail.com>
2019-01-14 17:16:31 +0000

    Test: Checks that if a top_up method is invoked with the top up value as the argument

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,8 +1,19 @@
 require 'Oystercard'
 
+
 RSpec.describe Oystercard do
   it 'Card has a default balance of 0' do
     card = Oystercard.new
     expect(card.balance).to eq(0)
   end
+
+context '#top_up' do
+  it 'checks that if a top_up method is invoked with the top up value as the argument' do
+    card = Oystercard.new
+    amount = double('some value')
+    expect(card.top_up(amount))
+  end
+end
+
+
 end
