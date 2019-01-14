f3632771f12f3ca91f25e5fd27ff4e7d4921d48c
Aiden Gleave <aidengleave@gmail.com>
2019-01-14 18:24:17 +0000

    Refactoring tests to be more implicit

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,25 +1,16 @@
 require 'Oystercard'
 
-
 RSpec.describe Oystercard do
-
   it 'Card has a default balance of 0' do
     card = Oystercard.new
     expect(card.balance).to eq(0)
   end
 
-context '#top_up' do
-  it 'checks that if a top_up method is invoked with the top up value as the argument' do
-    card = Oystercard.new
-    amount = 1
-    expect(card.top_up(amount))
-  end
+  context '#top_up' do
+    it { is_expected.to respond_to(:top_up).with(1).argument }
 
-  it 'can be topped up' do
-    card = Oystercard.new
-    amount = 1
-    expect(card.top_up(amount)).to eq(amount)
+    it 'can be topped up' do
+      expect { subject.top_up 1 }.to change { subject.balance }.by 1
+    end
   end
-end
-
 end
