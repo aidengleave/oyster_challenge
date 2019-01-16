64dfaea977772bc7539f45536a62e81c91afebc1
Aiden Gleave <aidengleave@gmail.com>
2019-01-16 16:09:16 +0000

    Wrote test for top up storing exit station.

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -10,33 +10,38 @@ RSpec.describe Oystercard do
   end
 
   context 'Card behaviour during the journey' do
-    let (:station) {double starting_station}
+    let (:starting_station) {double starting_station}
+    let (:exit_station) {double exit_station}
+
     it 'touch in saves starting station' do
       subject.top_up(2)
       expect(subject).to respond_to(:touch_in).with(1).argument
+    end
+    it 'touch out saves exit station' do
+      expect(subject).to respond_to(:touch_out).with(1).argument
 
     end
     it 'User can touch out' do
       subject.top_up(2)
-      subject.touch_in(:station)
-      expect { subject.touch_out }. to change { subject.starting_station }.to nil
+      subject.touch_in(:starting_station)
+      expect { subject.touch_out(:exit_station) }. to change { subject.starting_station }.to nil
     end
 
     it 'card deducts fare on touch out' do
       fare = Oystercard::MINIMUM_FARE * -1
       subject.top_up(2)
-      subject.touch_in(:station)
-      expect { subject.touch_out }.to change { subject.balance }.by(fare)      
+      subject.touch_in(:starting_station)
+      expect { subject.touch_out(:exit_station) }.to change { subject.balance }.by(fare)      
     end
 
     it 'Touch in changes journey status' do
       subject.top_up(2)
-      subject.touch_in(:station)
+      subject.touch_in(:starting_station)
       expect(subject.starting_station).not_to eq(nil)
     end
 
     it 'raises error if insufficient funds' do
-      expect { subject.touch_in(:station) }.to raise_error('insufficient funds')
+      expect { subject.touch_in(:starting_station) }.to raise_error('insufficient funds')
     end
   end
 end
