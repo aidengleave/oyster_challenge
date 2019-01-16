dfbd07869bbfbeb04247875613d0975a4f468ce9
Aiden Gleave <aidengleave@gmail.com>
2019-01-16 14:22:49 +0000

    Test for fare deduction

--- spec/Oystercard_spec.rb
+++ spec/Oystercard_spec.rb
@@ -1,28 +1,18 @@
 require 'Oystercard'
 
 RSpec.describe Oystercard do
-  it 'Card has a default balance of 0' do
-    card = Oystercard.new
-    expect(card.balance).to eq(0)
-  end
 
   describe '#top_up' do
-    it { is_expected.to respond_to(:top_up).with(1).argument }
-
-    it 'can be topped up' do
-      expect { subject.top_up 1 }.to change { subject.balance }.by 1
-    end
-
-    it 'Raises an error if balance exceeds £90' do
+    it 'Raises an error if balance exceeds the limit' do
       limit = Oystercard::LIMIT
       expect { subject.top_up limit }.to raise_error "You're over the limit #{limit}"
     end
   end
 
   describe '#deduct_fare' do
-    it { is_expected.to respond_to(:deduct_fare).with(1).argument }
-
+    
     it 'deducts a fare from the balance' do
+      subject.top_up(4)
       expect { subject.deduct_fare 4 }.to change { subject.balance }.by -4
     end
   end
@@ -32,6 +22,13 @@ RSpec.describe Oystercard do
       subject.top_up(2)
       subject.touch_in
       expect { subject.touch_out }. to change { subject.journey }.to false
+    end
+
+    it 'card deducts fare on touch out' do
+      fare = Oystercard::MINIMUM_FARE * -1
+      subject.top_up(2)
+      subject.touch_in
+      expect { subject.touch_out }.to change { subject.balance }.by(fare)      
     end
 
     it 'Touch in changes journey status' do
